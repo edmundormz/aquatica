158a5fc7b1a16c8677dd7714ceab4e9e8f21a61b
hector4 <hector.edmundo.ramirez.gomez@intel.com>
2018-11-04 21:48:57 -0600

    MarkI

---
+++ Marks/mark_I/mark_I.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 4         //D2
+#define RST_PIN 5        //D1
+
+MFRC522 mfrc522(SS_PIN,RST_PIN);
+
+int statuss = 0;
+int out = 0;
+
+void setup(){
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
+
+void loop(){
+  //Look for cards
+  if (!mfrc522.PICC_IsNewCardPresent()){
+    return;
+  }
+  //Select one of the cards
+  if(!mfrc522.PICC_ReadCardSerial()){
+    return;
+  }
+
+  String content = "";
+  byte letter;
+  for(byte i = 0; i < mfrc522.uid.size; i++){
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  Serial.println();
+  
+}
