7f1a61449bfc585400bab343c257b9334cde64b1
Edmundo Ramirez <edmundormz@gmail.com>
2019-03-09 14:14:05 -0600

    Adding basic OTA working script

--- 101s/00_blink/00_blink.ino
+++ 101s/00_blink/00_blink.ino
@@ -10,15 +10,15 @@
 */
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(2, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  digitalWrite(2, LOW);   // Turn the LED on (Note that LOW is the voltage level
   // but actually the LED is on; this is because
   // it is active low on the ESP-01)
   delay(500);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(2, HIGH); // Turn the LED off by making the voltage HIGH
   delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
